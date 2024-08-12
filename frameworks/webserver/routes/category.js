91c37cbfa6e8788f0b7e49509efd9bfda4f5089b
nampx <xuannamhuuvan2015@gmail.com>
2024-08-12 15:55:15 +0700

    config swagger category

--- frameworks/webserver/routes/category.js
+++ frameworks/webserver/routes/category.js
@@ -19,12 +19,43 @@ export default function categoryRouter(express, redisClient) {
   );
 
   // GET endpoints
+  /**
+   * @openapi
+   * '/api/v1/category':
+   *  get:
+   *     tags:
+   *     - Category
+   *     summary: Get category
+   *     security:
+   *       - BearerAuth: []
+   *     responses:
+   *       200:
+   *         description: Success
+   */
   router
     .route('/')
     .get(
       [authMiddleware, redisCachingMiddleware(redisClient, 'category')],
       controller.fetchAllCategories
     );
+  /**
+   * @openapi
+   * '/api/v1/category/{id}':
+   *  get:
+   *     tags:
+   *     - Category
+   *     summary: Get category
+   *     parameters:
+   *      - name: id
+   *        in: path
+   *        description: The unique id of the category
+   *        required: true
+   *     security:
+   *       - BearerAuth: []
+   *     responses:
+   *       200:
+   *         description: Success
+   */
   router
     .route('/:id')
     .get(
@@ -33,12 +64,79 @@ export default function categoryRouter(express, redisClient) {
     );
 
   // POST endpoints
+  /**
+   * @openapi
+   * '/api/v1/category':
+   *  post:
+   *     tags:
+   *     - Category
+   *     summary: Create a category
+   *     requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *           schema:
+   *            type: object
+   *            required:
+   *              - title
+   *            properties:
+   *              title:
+   *                type: string
+   *                default: category1
+   *     responses:
+   *      200:
+   *        description: Created
+   */
   router.route('/').post(authMiddleware, controller.addNewCategory);
 
   // PUT endpoints
+  /**
+   * @openapi
+   * '/api/v1/category/{id}':
+   *  put:
+   *     tags:
+   *     - Category
+   *     summary: Update a category
+   *     parameters:
+   *      - name: id
+   *        in: path
+   *        description: The unique id of the category
+   *        required: true
+   *     requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *           schema:
+   *            type: object
+   *            required:
+   *              - title
+   *            properties:
+   *              title:
+   *                type: string
+   *                default: category1
+   *     responses:
+   *      200:
+   *        description: Created
+   */
   router.route('/:id').put(authMiddleware, controller.updateCategoryById);
 
   // DELETE endpoints
+  /**
+   * @openapi
+   * '/api/v1/category/{id}':
+   *  delete:
+   *     tags:
+   *     - Category
+   *     summary: Remove category by id
+   *     parameters:
+   *      - name: id
+   *        in: path
+   *        description: The unique id of the category
+   *        required: true
+   *     responses:
+   *      200:
+   *        description: Success
+   */
   router.route('/:id').delete(authMiddleware, controller.deleteCategoryById);
 
   return router;
